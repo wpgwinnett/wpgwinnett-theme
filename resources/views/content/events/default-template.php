2e28797ba690c89e1eda996ed392b50d79ba87e5
ilenejohnson <ilenej@comcast.net>
2018-10-10 15:45:22 -0400

    added tribe events before html and after html commands

--- resources/views/content/events/default-template.php
+++ resources/views/content/events/default-template.php
@@ -7,10 +7,9 @@
 <h1 class="entry__title"><?php esc_html_e( 'Calendar') ?> </h1>
 
 <div id="tribe-events-pg-template" class="tribe-events-pg-template">
-
-	
+	<?php tribe_events_before_html(); ?>
 	<?php tribe_get_view(); ?>
-	
+	<?php tribe_events_after_html(); ?>
 	
 </div> <!-- #tribe-events-pg-template -->
 
