dbc819188bddbeb3ca3fd9836955f5be86abb046
ilenejohnson <ilenej@comcast.net>
2018-10-14 20:33:52 -0400

    a added events-home.php

---
+++ resources/views/content/events/events-home.php
@@ -0,0 +1,22 @@
+<div class="app-content">
+
+	<?php Hybrid\View\display( 'header', Hybrid\Template\hierarchy() ) ?>
+
+<main id="main" class="app-main">
+our events calendar home
+<h1 class="entry__title"><?php esc_html_e( 'Calendar') ?> </h1>
+
+<div id="tribe-events-pg-template" class="tribe-events-pg-template">
+	<?php tribe_events_before_html(); ?>
+	<?php tribe_get_view(); ?>
+	<?php tribe_events_after_html(); ?>
+	
+</div> <!-- #tribe-events-pg-template -->
+
+
+
+</main>
+
+	
+	<?php Hybrid\View\display( 'footer', Hybrid\Template\hierarchy() ); ?>
+</div>
