bd96620e1d2260ac5dbb589697542b31740e0ef2
Charles Loehle <clguitar1@gmail.com>
2018-11-04 13:33:33 -0500

    in functions.php replaced hard-coded reference to wp-content with get_template_directory();

--- functions.php
+++ functions.php
@@ -50,7 +50,7 @@ add_action( 'hybrid/templates/register', function( $templates ) {
 function tribe_filter_template_paths ( $file, $template ) {
 	/*  This is the file path for when we go on the server 
 	*/
-		$custom_file_path =  ABSPATH . 'wp-content/themes/wpgwinnett-theme/resources/views/content/events/' . $template;
+		$custom_file_path = get_template_directory() . '/resources/views/content/events/' . $template;
 		
 	 /*  this is the file path for when you are on your local server */
 	
